94fee6b4036a3bdbb6e4a8cab86bbf590dbe9387
Felipe Santos <carlosfsp@dcc.ufrj.br>
2025-05-20 18:25:48 -0300

    MVP usando Django + classe Cliente criadas

---
+++ credit_mvp/core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    idade = models.IntegerField()
+    salario_mensal = models.FloatField()
+    score_credito = models.FloatField()
+    tempo_emprego_anos = models.FloatField()
+    estado_civil = models.CharField(max_length=20)
+    possui_imovel = models.BooleanField()
+    possui_veiculo = models.BooleanField()
+    qtd_cartoes_credito = models.IntegerField()
+    historico_inadimplencia = models.BooleanField()
+    valor_credito_solicitado = models.FloatField()
+    dividas_banco_A = models.FloatField()
+    dividas_banco_B = models.FloatField()
+    dividas_banco_C = models.FloatField()
+    dividas_totais = models.FloatField()
+
+    # Campos que serão preenchidos após análise
+    score_risco = models.FloatField(null=True, blank=True)
+    aprovado_previsto = models.BooleanField(null=True, blank=True)
+    data_analise = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"Cliente {self.id} - Score: {self.score_risco:.2f}" if self.score_risco else f"Cliente {self.id}"
---
+++ credit_mvp/credit_mvp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for credit_mvp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'credit_mvp.settings')
+
+application = get_asgi_application()
